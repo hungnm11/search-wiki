3f7a3c80fc35335cc1cbb656f2435e80bc48ed11
Hung Nguyen <hung.nguyen@hotmail.com.vn>
2024-07-12 20:36:29 +0700

    unit test INPUT

--- src/components/Input/index.test.tsx
+++ src/components/Input/index.test.tsx
@@ -1,12 +1,27 @@
-import { render } from '@testing-library/react';
-import Input from '.'; // Adjust the import path
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Input from '.';
 
 describe('Input Component', () => {
-  let props: any;
-  let sut;
+  it('renders input with given placeholder', () => {
+    const placeholderText = 'Enter text here';
+    render(<Input placeholder={placeholderText} />);
 
-  it('Should match snapshot', () => {
-    sut = render(<Input {...props} />);
-    expect(sut).toMatchSnapshot();
+    const inputElement = screen.getByPlaceholderText(placeholderText);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it('spreads rest props', () => {
+    render(<Input data-testid='input-test-id' />);
+
+    const inputElement = screen.getByTestId('input-test-id');
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it('applies custom class', () => {
+    render(<Input className='custom-class' />);
+
+    const inputElement = screen.getByRole('textbox');
+    expect(inputElement).toHaveClass('input-field test custom-class');
   });
 });
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,9 +4,14 @@ import React, { HTMLProps } from 'react';
 
 const Input: React.FC<HTMLProps<HTMLInputElement>> = ({
   placeholder,
+  className = '',
   ...rest
 }) => (
-  <input className='input-field test' placeholder={placeholder} {...rest} />
+  <input
+    className={`input-field test ${className}`}
+    placeholder={placeholder}
+    {...rest}
+  />
 );
 
 export default Input;
